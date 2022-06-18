557c377171c36bac1e5888100fd8f83c9bd4a24c
nativeAble <nativeable@gmail.com>
2022-06-18 11:07:15 -0500

    mobmenu

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -18,18 +18,5 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 </head>
 <body id="body" class="body">
-<div id="mobileMenu" class="flex flex-col justify-center items-center menu">
-        <div id="hambMenuClose" clas="">
-            <i class="fa-solid fa-xmark cursor-pointer"></i>
-        </div>
-        <ul class="flex flex-col justify-center text-center">
-            <li><a href="" class="flex justify-center text-white">Interescolar</a></li>
-            <li><a href="" class="flex justify-center text-white">Race</a></li>
-            <li><a href="" class="flex justify-center text-white">Clasico</a></li>
-        </ul>
-        <div id="buttonsMobile" class="flex flex-col items-center justify-center">
-            <li class="list-none"><a href="./register.php" class="flex justify-center text-white">Registrate</a></li>
-            <li class="list-none"><a href="./login.php" class="flex justify-center uppercase italic text-white">Iniciar sesion</a></li>
-        </div>
-    </div>
+<x-mobilemenu/>
 <main id="main" class="container h-max overflow-hidden mx-auto box-border">
