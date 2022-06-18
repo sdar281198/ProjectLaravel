f6d7176b7b350891c5b50d0caafc0d58b927a953
nativeAble <nativeable@gmail.com>
2022-06-18 10:51:37 -0500

    mobilemenu

--- public/js/backslider.js
+++ public/js/backslider.js
@@ -37,6 +37,18 @@
 // }
 
 /*Scroll reveal effects */
+const menuOpen = document.getElementById('hambMenu');
+const menuClose = document.getElementById('hambMenuClose');
+const headerOverlay = document.getElementById('body');
+const mobileMenu = document.getElementById('mobileMenu');
+menuOpen.addEventListener('click', () => {
+    mobileMenu.classList.add('menu-opened');
+    headerOverlay.classList.add('menu-opened');
+});
+menuClose.addEventListener('click', () => {
+    mobileMenu.classList.remove('menu-opened');
+    headerOverlay.classList.remove('menu-opened');
+});
 window.sr = ScrollReveal();
 sr.reveal('#header',{
     duration: 3000,
