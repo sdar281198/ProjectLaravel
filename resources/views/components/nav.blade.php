b17a8cd1f834307fb5c526c4b6c5eb75712b19de
nativeAble <nativeable@gmail.com>
2022-06-30 17:21:33 -0500

    links

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -8,8 +8,8 @@
             <li class="flex grow text-center"><a href="" class="flex justify-center grow">Clásico</a></li>
         </ul>
         <div id="buttons" class="flex items-center justify-end grow">
-            <li class="button-img list-none"><a href="./register.php" class="flex justify-end"><img src="../img/BOTON REGISTRARSE-07.png" alt=""></a></li>
-            <li class="button-sign list-none"><a href="./login.php" class="flex justify-end uppercase italic">Iniciar sesión</a></li>
+            <li class="button-img list-none"><a href="./register" class="flex justify-end"><img src="../img/BOTON REGISTRARSE-07.png" alt=""></a></li>
+            <li class="button-sign list-none"><a href="./login2" class="flex justify-end uppercase italic">Iniciar sesión</a></li>
         </div>
         <div id="hambMenu">
         <i class="fa-solid fa-bars"></i>
